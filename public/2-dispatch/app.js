0239d41515ea8f2035399b92709ce4551fbd37f0
jotasolano <jsolano91@gmail.com>
2017-03-02 16:50:35 -0500

    :rocket: part 1 of dispatch complete

--- public/2-dispatch/app.js
+++ public/2-dispatch/app.js
@@ -1,6 +1,8 @@
 var W = d3.select('#plot1').node().clientWidth,
 	H = d3.select('#plot1').node().clientHeight;
 
+var dispatch = d3.dispatch('update');
+
 //SVG 1
 var svg1 = d3.select('#plot1').append('svg')
 	.attr('width',W)
@@ -10,11 +12,16 @@ var svg1 = d3.select('#plot1').append('svg')
 		d3.select(this).select('.pointer')
 			.attr('cx',xy[0])
 			.attr('cy',xy[1]);
-		//Hint: dispatch.call('mouse:move',this,xy)
+		dispatch.call('update', this, xy);
 	});
 svg1.append('circle').attr('class','pointer') 
 	.attr('r',5)
 	.style('fill','red');
+
+dispatch.on('update.svg1', function(xy){
+	// d3.selectAll('.pointer').attr('cx', xy[0]).attr('cy', xy[1]);
+	d3.select('#plot2').selectAll('.pointer').attr('cx', xy[0]).attr('cy', xy[1]);
+});
 
 //Hint: dispatch.on('mouse:move.svg1',function(xy){...})
 //Note the use of .svg1: we add a "name space" to the "mouse:move" event to attach multiple event listeners to the same event
@@ -28,10 +35,18 @@ var svg2 = d3.select('#plot2').append('svg')
 		d3.select(this).select('.pointer')
 			.attr('cx',xy[0])
 			.attr('cy',xy[1]);
+
+		dispatch.call('update', this, xy)
 	});
 svg2.append('circle').attr('class','pointer') 
 	.attr('r',5)
 	.style('fill','red');
+
+
+dispatch.on('update.svg2', function(xy){
+	// d3.selectAll('.pointer').attr('cx', xy[0]).attr('cy', xy[1]);
+	d3.select('#plot1').selectAll('.pointer').attr('cx', xy[0]).attr('cy', xy[1]);
+});
 
 //Can you think of a concise way to make all the dots move in the same way across all .plot elements?
 //Hint below:
